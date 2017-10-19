51467f680aef6d4431b109067234e0d7185975cc
ira-che <ira.chernomorets@gmail.com>
2017-10-19 14:38:01 +0300

    change banner link sign up

--- wp-content/themes/new_ielts_theme/front-page.php
+++ wp-content/themes/new_ielts_theme/front-page.php
@@ -124,34 +124,17 @@ if(is_user_logged_in()) {
     foreach ($posts_array as $post) : setup_postdata($post);
         echo '<div class="top-header-offer" style="background-image: url(' . get_the_post_thumbnail_url() . ');">';
             echo '<div class="center-horiz-vert">';
-            the_content();
+                the_content();
+                echo '<a href="' . get_permalink( get_page_by_path('under-construction')->ID ) . '">' . _("Sign up for 50$/mo") . '</a>';
             echo '</div>';
         echo '</div>';
     endforeach;
     wp_reset_postdata();
 
     echo '<main class="front-page-text">';
-//        $menu_name = 'reading_section_completion';
-//        $locations = get_nav_menu_locations();
-//
-//        if( $locations && isset($locations[ $menu_name ]) ){
-//            $menu = wp_get_nav_menu_object( $locations[ $menu_name ] ); // получаем объект меню
-//            $menu_items = wp_get_nav_menu_items( $menu ); // получаем элементы меню
-//
-//            $menu_list = '<nav id="menu-' . $menu_name . '" class="section-menu">';
-//            foreach ( $menu_items as $key => $menu_item ){
-//                $menu_list .= '<a href="' . $menu_item->url . '" class="menu-item '.( ($menu_item->object_id == $direct_parent) ? 'active' : '' ).'">' . $menu_item->title;
-//                $menu_list .= '</a>';
-//            }
-//            $menu_list .= '</nav>';
-//
-//            echo $menu_list;
-//        }
-
         while (have_posts()) : the_post();
             the_content();
         endwhile;
-
     echo '</main>';
 
     echo '<div class="front-page-reasons">';
