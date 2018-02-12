a05609c81f56c5c312ca451733bc5d08d3ff576a
ira.che <ira.chernomorets@gmail.com>
2018-02-12 18:30:07 +0200

    try to solve the problem with permission for microphone on record-functions.js

--- wp-content/themes/new_ielts_theme/js/record-functions.js
+++ wp-content/themes/new_ielts_theme/js/record-functions.js
@@ -200,7 +200,20 @@ function init(){
 
 init();
 
-var ctx = new AudioContext();
+// var ctx = new AudioContext();
+
+var AudioContext = window.AudioContext // Default
+    || window.webkitAudioContext // Safari and old versions of Chrome
+    || false;
+
+if (AudioContext) {
+    // Do whatever you want using the Web Audio API
+    var ctx = new AudioContext;
+} else {
+    // Web Audio API is not supported
+    // Alert the user
+    console.log("Sorry, but the Web Audio API is not supported by your browser. Please, consider upgrading to the latest version or downloading Google Chrome or Mozilla Firefox");
+}
 
 function showDuration(){
     $("audio").each(function(){
